b6ff17af444302a17801a1e32f2e7b31e117b612
thomas mutavi <mathiasthomas17@gmail.com>
2024-08-01 21:39:07 +0300

    crud

--- projects/forms.py
+++ projects/forms.py
@@ -3,4 +3,5 @@ from .models import Project
 class ProjectModelForm(ModelForm):
     class Meta:
         model = Project
-        fields = '__all__'
+        # fields = '__all__'
+        fields = ['title','description','demo_link','souce_link','tag']
--- projects/views.py
+++ projects/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project,Tag,Review
 from .forms import  ProjectModelForm
+from django.shortcuts import redirect
 
 
 
@@ -18,5 +19,33 @@ def project(request,pk):
 
 def createProject(request):
     form = ProjectModelForm()
+    if request.method =='POST':
+        # print(request.POST)
+        form = ProjectModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
     context = {'form':form}
     return render(request,'projects/projects_form.html',context)
+
+
+
+def updateProject(request,pk):
+    project = Project.objects.get(id = pk)
+    form = ProjectModelForm(instance=project)
+    if request.method =='POST':
+        # print(request.POST)
+        form = ProjectModelForm(request.POST,instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context = {'form':form}
+    return render(request,'projects/projects_form.html',context)
+
+def deleteProject(request,pk):
+    project = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    context = {'object':project}
+    return render(request,'projects/delete.html',context)
