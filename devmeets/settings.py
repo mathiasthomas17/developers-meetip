8019e79632b9db3a5ce079195ae15f17d7fd9e08
mathiasthomas <mathiasthomas17@gmail.com>
2024-08-03 22:24:23 +0300

    statics

--- devmeets/settings.py
+++ devmeets/settings.py
@@ -121,6 +121,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+
+# Statics ===============NEW
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
