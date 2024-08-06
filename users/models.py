c39c24116f775fd4b5136daa848984634a9b39d8
mathiasthomas <mathiasthomas17@gmail.com>
2024-08-06 21:44:29 +0300

    profiles

--- users/models.py
+++ users/models.py
@@ -7,6 +7,8 @@ import uuid
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,null=True,blank = True)
     username = models.CharField(max_length=200,blank = True,null = True)
+    location = models.CharField(max_length=200,blank = True,null = True)
+
     name= models.CharField(max_length=200,blank = True,null = True)
     email = models.EmailField(max_length=500,blank=True,null= True)
     short_intro = models.CharField(max_length=200,null=True,blank=True)
@@ -25,4 +27,12 @@ class Profile(models.Model):
     def __str__(self):
         return str(self.user.username)
 
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile,models.CASCADE,null=True,blank=True)
+    name = models.CharField(max_length=200,blank = True,null = True)
+    description = models.TextField(null=True,blank=True)
+    id =  models.UUIDField(default = uuid.uuid4,unique = True,primary_key = True, editable = False)
+
+    def __str__(self):
+        return self.name
 
--- users/views.py
+++ users/views.py
@@ -6,3 +6,11 @@ def profiles(request):
     context = {'profiles':profiles}
     return render(request, 'users/profiles.html',context)
 
+def userProfile(request,pk):
+    profile = Profile.objects.get(id=pk)
+    # skils with description
+    topSkills  =profile.skill_set.exclude(description__exact = '')
+    # Skills wwith descr
+    otherSkills = profile.skill_set.filter(description = '')
+    context = {'profile':profile,'topSkills':topSkills,'otherSkills':otherSkills}
+    return render(request, 'users/user-profile.html',context)
